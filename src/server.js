4ea71642cbd2150d94e6079e723246e20d29e712
Jay <jordanbell321@gmail.com>
2025-03-11 02:25:17 +0000

    feat: Add axios and cookie-parser dependencies to package.json and package-lock.json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
